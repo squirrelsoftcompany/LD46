eb8e001b0a443870e539c103bfc6b6458932b04c
christophe.claustre <christophe.claustre@diginext.fr>
2020-04-19 01:59:41 +0200

    [FCT] add of the TurnManager (+ dummy for test and example)

---
+++ Assets/Scripts/TurnManager/Debug/Dummy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Turn
+{
+
+    public class Dummy : MonoBehaviour
+    {
+        string messageStart = "Doing its turn...";
+        string messageStop = "Done.";
+        public GameEventSystem.GameEvent thingDoneEvent;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void DoYourTurn()
+        {
+            StartCoroutine(MyCoroutine());
+
+            Debug.Log(ToString() + " " + messageStart);
+        }
+
+        IEnumerator MyCoroutine()
+        {
+            yield return new WaitForSeconds(5);
+
+            Debug.Log(ToString() + " " + messageStop);
+            thingDoneEvent.Raise();
+        }
+    }
+
+}
---
+++ Assets/Scripts/TurnManager/TurnManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Turn
+{
+    [System.Serializable]
+    public struct Part
+    {
+        public List<GameObject> objects;
+    }
+
+    public class TurnManager : MonoBehaviour
+    {
+        public List<Part> parts;
+        int _partIndex = 0;
+        int _turn = 0;
+        int _objectTurnNotFinished = 0;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Next();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void AddObject(GameObject go, int part)
+        {
+            Debug.Assert(part >= 0 && part < parts.Count);
+            parts[part].objects.Add(go);
+        }
+
+        public void OneObjectFinishedItsTurnSlot()
+        {
+            _objectTurnNotFinished--;
+            if (_objectTurnNotFinished <= 0)
+            {
+                Next();
+            }
+        }
+
+        public void Next()
+        {
+            _objectTurnNotFinished = 0;
+            foreach (GameObject go in parts[_partIndex].objects)
+            {
+                go.SendMessage("DoYourTurn");
+                _objectTurnNotFinished++;
+            }
+
+            _partIndex = (_partIndex + 1) % parts.Count;
+            if (_partIndex == 0)
+                _turn++;
+        }
+    }
+
+}
