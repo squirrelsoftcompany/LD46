6e2adf90d17eee301e1984cbaadfa5ba10172474
OroshiX <oroshix@users.noreply.github.com>
2020-04-19 21:46:40 +0200

    [QA] Cleanup

--- Assets/Scripts/camera/UILookCamera.cs
+++ Assets/Scripts/camera/UILookCamera.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+
 // ReSharper disable RedundantDefaultMemberInitializer
 
 namespace camera {
-    public class UILookCamera : MonoBehaviour {
+    public class UiLookCamera : MonoBehaviour {
         [SerializeField] private Camera myCamera = default;
 
         private void Update() {
-            transform.LookAt(transform.position + myCamera.transform.rotation * Vector3.forward,
-                myCamera.transform.rotation * Vector3.up);
+            var rotation = myCamera.transform.rotation;
+            transform.LookAt(transform.position + rotation * Vector3.forward,
+                rotation * Vector3.up);
         }
     }
 }
