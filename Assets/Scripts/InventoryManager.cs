7fcf627a99215193b65776c1c76b3bc1b5cdc1de
Xavier Chalut <xavier.chalut@gmail.com>
2020-04-20 00:17:23 +0200

    [FCT] Add Dora and basic inventory (Food and Water)

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance
+    {
+        get
+        {
+            return m_instance;
+        }
+    }
+    public int Food
+    {
+        get
+        {
+            return mFood;
+        }
+        set
+        {
+            mFood = value;
+        }
+    }
+    public int Water
+    {
+        get
+        {
+            return mWater;
+        }
+        set
+        {
+            mWater = value;
+        }
+    }
+
+    public static InventoryManager m_instance = null;
+    [SerializeField] private int mFood = 0;
+    [SerializeField] private int mWater = 0;
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_instance != null)
+            Destroy(gameObject);
+        else
+            m_instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addFood(int pFood)
+    {
+        mFood = mFood + pFood;
+    }
+
+    public void addWater(int pWater)
+    {
+        mWater = mWater + pWater;
+    }
+
+    public void subFood(int pFood)
+    {
+        mFood = (mFood - pFood)<0 ? 0 : (mFood - pFood);
+    }
+
+    public void subWater(int pWater)
+    {
+        mWater = (mWater - pWater)<0 ? 0 : (mWater - pWater);
+    }
+}
