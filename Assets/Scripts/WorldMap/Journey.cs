0b72c2be549fd76e4aa83acaa4026ae6355fd51c
Xavier Chalut <xavier.chalut@gmail.com>
2020-04-19 22:31:45 +0200

    [FCT] Add world map basic features (show map, move next point)

---
+++ Assets/Scripts/WorldMap/Journey.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Journey : MonoBehaviour
+{
+    [SerializeField]
+    private List<PointOfInterest> mPOIList;
+    [SerializeField]
+    private GameObject mWorldCamel;
+    [SerializeField]
+    private int mCurrentPOIIndex = 0;
+    private bool mMapVisible = true;
+
+    //Inspector variables for test
+    [SerializeField]
+    private bool mShow = false;
+
+    //TODO : Verifier si l'inventaire est suffisant pour pouvoir aller au point suivant
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void GoToNextPOI()
+    {
+        mCurrentPOIIndex++;
+        mWorldCamel.GetComponent<Animator>().SetInteger("position", mCurrentPOIIndex);
+        //Change current POI look here if you want
+    }
+
+    public void GoInPOI()
+    {
+        return;
+        //Load and show map
+    }
+
+    public void ShowMap(bool pShow)
+    {
+        transform.GetComponent<Image>().enabled = pShow;
+        foreach (Image child in GetComponentsInChildren<Image>())
+        {
+            child.enabled = pShow;
+        }
+        mMapVisible = pShow;
+    }
+
+    public PointOfInterest GetCurrentPOI()
+    {
+        return mPOIList[mCurrentPOIIndex];
+    }
+
+    public void Update()
+    {
+        if (mMapVisible != mShow)
+            ShowMap(mShow);
+    }
+}
---
+++ Assets/Scripts/WorldMap/PointOfInterest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PointOfInterest : MonoBehaviour
+{
+    [SerializeField] private int mFoodCost = 0;
+    [SerializeField] private int mWaterCost = 0;
+
+    public int FoodCost
+    {
+        get
+        {
+            return mFoodCost;
+        }
+        set
+        {
+            mFoodCost = value;
+        }
+    }
+
+    public int WaterCost
+    {
+        get
+        {
+            return mWaterCost;
+        }
+        set
+        {
+            mWaterCost = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
