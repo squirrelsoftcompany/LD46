23d0c0e32edce9e731fc957e3f4d7f24a5196d7e
OroshiX <oroshix@users.noreply.github.com>
2020-04-20 03:18:38 +0200

    [FIX]? Turn manager should now avoid stack overflow

--- Assets/Scripts/TurnManager/TurnManager.cs
+++ Assets/Scripts/TurnManager/TurnManager.cs
@@ -2,61 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Turn
-{
+namespace Turn {
     [System.Serializable]
-    public struct Part
-    {
+    public struct Part {
         public List<GameObject> objects;
     }
 
-    public class TurnManager : MonoBehaviour
-    {
+    public class TurnManager : MonoBehaviour {
         public List<Part> parts;
         int _partIndex = 0;
         int _turn = 0;
         int _objectTurnNotFinished = 0;
+        private bool readyToMoveOn = false;
+        private bool shouldRelaunchImmediately = false;
 
         // Start is called before the first frame update
-        void Start()
-        {
+        void Start() {
             Next();
         }
 
         // Update is called once per frame
-        void Update()
-        {
+        void Update() { }
 
-        }
-
-        public void AddObject(GameObject go, int part)
-        {
+        public void AddObject(GameObject go, int part) {
             Debug.Assert(part >= 0 && part < parts.Count);
             parts[part].objects.Add(go);
         }
 
-        public void OneObjectFinishedItsTurnSlot()
-        {
+        public void OneObjectFinishedItsTurnSlot() {
             _objectTurnNotFinished--;
-            if (_objectTurnNotFinished <= 0)
-            {
+            if (!readyToMoveOn && _objectTurnNotFinished <= 0) {
+                shouldRelaunchImmediately = true;
+                return;
+            }
+
+            if (_objectTurnNotFinished <= 0) {
+                shouldRelaunchImmediately = false;
                 Next();
             }
         }
 
-        public void Next()
-        {
+        public void Next() {
+            readyToMoveOn = false;
             _objectTurnNotFinished = 0;
-            foreach (GameObject go in parts[_partIndex].objects)
-            {
-                go.SendMessage("DoYourTurn");
+            foreach (GameObject go in parts[_partIndex].objects) {
                 _objectTurnNotFinished++;
+                go.SendMessage("DoYourTurn");
             }
 
             _partIndex = (_partIndex + 1) % parts.Count;
             if (_partIndex == 0)
                 _turn++;
+            readyToMoveOn = true;
+            if (shouldRelaunchImmediately) {
+                shouldRelaunchImmediately = false;
+                Next();
+            }
         }
     }
-
 }
