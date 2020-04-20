b158f0dafd354a5b99ec98371e8185e3c57a8e64
OroshiX <oroshix@users.noreply.github.com>
2020-04-20 23:50:15 +0200

    [QA] cleanup

--- Assets/Scripts/controllers/PositionedLivingThing.cs
+++
@@ -1,8 +0,0 @@
-using hex;
-using UnityEngine;
-
-namespace controllers {
-    public abstract class PositionedLivingThing : MonoBehaviour {
-        public abstract HexCoordinates Position { get; }
-    }
-}
