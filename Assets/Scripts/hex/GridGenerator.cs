965d84afdec86bb3fbde3adf9cdd28141183d51a
christophe.claustre <christophe.claustre@diginext.fr>
2020-04-20 03:18:19 +0200

    [WIP] building generation

--- Assets/Scripts/hex/GridGenerator.cs
+++ Assets/Scripts/hex/GridGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SixWay = hex.HexCoordinates.SixWay;
 
 public class GridGenerator
 {
@@ -15,33 +16,72 @@ public class GridGenerator
 
     public void Generate()
     {
-        GenerateBuilding(choose(_buildings), new hex.HexCoordinates(3,3));
+        GenerateBuilding(choose(_buildings), new hex.HexCoordinates(3,3), 3, SixWay.ZMinus, new[] { 0, -1, 0});
+        GenerateBuilding(choose(_buildings), new hex.HexCoordinates(7,7), 2, SixWay.XMinus, new[] { 0, 0, 0 });
+        GenerateBuilding(choose(_buildings), new hex.HexCoordinates(0,11), 1, SixWay.XMinus, new[] { 2, 0, 0 });
     }
 
-    public void GenerateBuilding(GridGeneration.Building building, hex.HexCoordinates startCoords)
+    private void GenerateBuilding(GridGeneration.Building building, hex.HexCoordinates startCoords, int radius, SixWay doorWallOrientation, int[] offsetByAxis)
     {
-        GenerateTopping(choose(building.doors), startCoords);
-        GenerateTopping(choose(building.walls), startCoords.Xplus());
-        //GenerateTopping(choose(building.walls), startCoords.Xminus());
-        //GenerateTopping(choose(building.walls), startCoords.Zplus(), 60);
-        //GenerateTopping(choose(building.walls), startCoords.Zminus(), 60);
-        GenerateTopping(choose(building.walls), startCoords.Yplus(), 120);
-        //GenerateTopping(choose(building.walls), startCoords.Yminus(), 120);
+        Debug.Assert(offsetByAxis.Length == 3);
+        hex.HexCoordinates current = startCoords.moveAlongAxis((SixWay)(((int)doorWallOrientation + 4) % 6), radius);
+
+        SixWay way = doorWallOrientation;
+        current = current.moveAlongAxis(way, 1);
+
+        for (int j = 0; j < 6; j++)
+        {
+            int rotation = 60 * ((int)way - 1);
+
+            int max = radius + offsetByAxis[j % 3];
+            for (int i = 0; i < max - 1; i++)
+            {
+                if (j == 0 && i == Mathf.CeilToInt((max / 2.0f) - 1))
+                    GenerateTopping(choose(building.doors), current, rotation);
+                else
+                    GenerateTopping(choose(building.walls), current, rotation);
+                current = current.moveAlongAxis(way, 1);
+            }
+            GenerateTopping(choose(building.cornersConvex), current, rotation);
+
+            way = (SixWay)(((int)way + 1) % 6);
+            current = current.moveAlongAxis(way, 1);
+        }
+
+        FillTopping(building.inside, startCoords);
     }
 
-    public void GenerateTopping(GameObject topping, hex.HexCoordinates coordinates, int rotation = 0)
+    private void FillTopping(List<GameObject> toppings, hex.HexCoordinates start)
     {
+        GenerateTopping(choose(toppings), start);
+
+        var enumCount = System.Enum.GetNames(typeof(SixWay)).Length;
+        for (int w = 0; w < enumCount; w++)
+        {
+            var neighbor = start.moveAlongAxis((SixWay)w, 1);
+            if (_grid.CellAvailable(neighbor))
+                FillTopping(toppings, neighbor);
+        }
+    }
+
+    private void GenerateTopping(GameObject topping, hex.HexCoordinates coordinates, int rotation = 0)
+    {
+        hex.HexCell cell = _grid[coordinates];
+        if (cell != null)
+            return;
+        Debug.Assert(cell.available());
         _grid[coordinates].topping = GenerateSomething(topping, coordinates, rotation);
     }
 
-    public GameObject GenerateSomething(GameObject something, hex.HexCoordinates coordinates, int rotation = 0)
+    private GameObject GenerateSomething(GameObject something, hex.HexCoordinates coordinates, int rotation = 0)
     {
         var generated = GameObject.Instantiate(something, _grid[coordinates].gameObject.transform);
         generated.transform.localRotation = Quaternion.Euler(0, rotation, 0);
+        generated.transform.localPosition = Vector3.zero;
         return generated;
     }
 
-    public T choose<T>(List<T> ts)
+    private T choose<T>(List<T> ts)
     {
         Debug.Assert(ts.Count > 0);
         return ts[Random.Range(0, ts.Count)];
