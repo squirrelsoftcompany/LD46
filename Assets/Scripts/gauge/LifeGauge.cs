fe4fefa759541a9f685087a2c80d031b6f0b272a
OroshiX <oroshix@users.noreply.github.com>
2020-04-20 14:00:19 +0200

    [FCT] when hurt, trigger animation

--- Assets/Scripts/gauge/LifeGauge.cs
+++ Assets/Scripts/gauge/LifeGauge.cs
@@ -9,17 +9,22 @@ namespace gauge {
 
         [SerializeField] private int maxLife = 50;
         [SerializeField] private GameEvent onDie = default;
+        private Animator animator;
+        private static readonly int HURT = Animator.StringToHash("hurt");
 
         private void Awake() {
             if (Debug.isDebugBuild && onDie == null) {
                 Debug.LogWarning("onDie game event is null");
             }
 
+            animator = GetComponentInChildren<Animator>();
+
             life = maxLife;
         }
 
         public void loseLife(int amount) {
             if (amount < 0) return;
+            animator.SetTrigger(HURT);
             life -= amount;
             if (life < 0) {
                 onDie.Raise();
